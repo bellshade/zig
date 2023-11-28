fd95babedccfdc7ba9b24dca54e6d82e569ec198
slowy07 <slowy.arfy@gmail.com>
2023-11-28 23:34:42 +0700

    fix: menghapus
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- Basic/01_introduction/helloWorld.zig
+++ Basic/01_introduction/helloWorld.zig
@@ -1,5 +1,5 @@
 // import modul std ke dalam variabel `std`
-const std = @import("std").debug.print;
+const std = @import("std");
 
 // fungsi `main()` adalah fungsi utama, fungsi ini akan dipanggil saat program dijalankan
 pub fn main() void {

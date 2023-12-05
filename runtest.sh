dcfeb2df90d7d486d8948b8e6b6482de1c3f6352
arfy slowy <slowy.arfy@gmail.com>
2023-12-05 19:36:02 +0700

    feat: menambahkan algoritma gcd (#8)
    - menambahkan beberapa testing
    - membuat build zig untuk mengetest semua folder
    - membuat command prompt file and shell script untuk running semua test
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ runtest.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# file ini berisi scripting shell yang digynakan untuk membuat beberapa
+# perintah build test zig yang terdapat di folder selain dari folder
+# `basic`. jika ada salah satu folder terdapat beberapa file,
+# maka perintah ini sangat berguna untuk mengetest semua file zig
+# yang terdapat pada folder tersebut
+
+# membuat parameter zig build test
+ZIG_TEST_COMMAND='zig build test'
+
+# membuat argumen tambahan untuk informasi lebih lanjut dari
+# hasil compile dan test
+# perintah;
+# `-freference-trace yaitu mengecek trace referencsi per kompile yang error`
+argumen='--summary all -freference-trace'
+
+# lakukan testing salah satu folder dengan menggunakan perintah
+# yang sudah dibuat
+$ZIG_TEST_COMMAND -Dalgoritma=math/gcd $argumen
+
